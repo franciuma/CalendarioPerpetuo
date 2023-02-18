a635dc0909fca722f9d137a2fe4f46c7ccbfc828
Franci <franciuma@uma.es>
2023-02-18 19:35:08 +0100

    Dias de la semana fix

--- symfony-docker/src/Entity/Calendario.php
+++ symfony-docker/src/Entity/Calendario.php
@@ -24,13 +24,13 @@ class Calendario
     );
 
     public $diasSemana = array(
-        0 => 'Domingo',
-        1 => 'Lunes',
+        0 => 'Lunes',
         2 => 'Martes',
         3 => 'Miércoles',
         4 => 'Jueves',
         5 => 'Viernes',
-        6 => 'Sábado'
+        6 => 'Sábado',
+        7 => 'Domingo'
     );
 
     #[ORM\Id]
@@ -74,5 +74,4 @@ class Calendario
     {
         return $this->diasSemana;
     }
-    //FALTA DECLARARLO COMO VARIABLE PRIVATE.
 }
